13b0e10cd26a8cf858bee188d86e71e04fd74a4b
MatheusFSouza <matheusfab1202@gmail.com>
2025-07-08 15:57:45 -0300

    besteira

--- sitebrba/urls.py
+++ sitebrba/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.index, name='index'),
 ]
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
