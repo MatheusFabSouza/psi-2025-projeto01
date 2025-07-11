58f4934d72fbca13a2d7e42d9b4953a4a83865e3
MatheusFSouza <matheusfab1202@gmail.com>
2025-07-11 15:49:22 -0300

    mudanças em app, urls, views, fotos e tomara que tu morra gabriel

---
+++ config/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def inicio(request):
+    return render(request, "sitebrba/inicio.html")
+
+elenco_brba = [
+    {"nome": "Walter", "Idade (em serie)": 32, "personagem:" "vilão", "img:" "walter"},
+]
+
+def elencobrba(request):
+        context = {
+              "elencobrba": elenco_brba,
+        }
+        return render(request, "sitebrba/elenco.html", context)
+
+def sobre(request):
+      return render(request, "sitebrba/sobre.html")
